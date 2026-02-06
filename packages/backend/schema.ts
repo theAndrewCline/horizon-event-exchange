d7c61644f5ed6b7949b856a7805a1422ccff38a5
Andrew Cline <andrew.cline77@gmail.com>
2026-02-06 13:43:56 -0600

    feat: add message schemas for web socket communication

--- packages/backend/schema.ts
+++ packages/backend/schema.ts
@@ -1,5 +1,16 @@
 import { z } from "zod";
 
+//
+// ASSET
+//
+
+export const AssetSchema = z.object({
+  id: z.string().ulid(),
+  name: z.string(),
+  price: z.number(),
+  updatedAt: z.string(),
+});
+
 export const CreateAssetSchema = z.object({
   name: z.string().min(1),
   price: z.number().min(0),
@@ -26,3 +37,33 @@ export const DatabaseRowSchema = z
 export type CreateAssetInput = z.infer<typeof CreateAssetSchema>;
 export type UpdateAssetInput = z.infer<typeof UpdateAssetSchema>;
 export type DatabaseRow = z.infer<typeof DatabaseRowSchema>;
+
+//
+// MESSAGE
+//
+
+export const createAssetMessageSchema = z.object({
+  type: z.literal("CREATE_ASSET"),
+  input: CreateAssetSchema,
+});
+
+export const updateAssetMessageSchema = z.object({
+  type: z.literal("UPDATE_ASSET"),
+  id: z.string().ulid(),
+  input: UpdateAssetSchema,
+});
+
+export const currentAssetsMessageSchema = z.object({
+  type: z.literal("CURRENT_ASSETS"),
+  assets: z.array(AssetSchema),
+});
+
+export const MessageSchema = z.discriminatedUnion("type", [
+  createAssetMessageSchema,
+  updateAssetMessageSchema,
+  currentAssetsMessageSchema,
+]);
+
+export type CurrentAssetsMessage = z.infer<typeof currentAssetsMessageSchema>;
+export type CreateAssetMessage = z.infer<typeof createAssetMessageSchema>;
+export type UpdateAssetMessage = z.infer<typeof updateAssetMessageSchema>;
